bca0431ad9afa2f61eb0eedb3debb307380bc6b8
Amvongo2021 <82786025+Amvongo2021@users.noreply.github.com>
2021-04-18 21:41:00 -0400

    Create Inventory.sh

---
+++ Inventory.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+## DESCRIPTION: SYSTEM INVENTORY
+## AUTHOR: Amvongo
+
+echo "This is my first system inventory"
+
+echo -e "\nchecking cpu info\n"
+sleep 3
+lscpu
+
+echo -e "\nchecking the memory info\n"
+sleep 3
+free -m
+
+echo -e  "\nchecking the kernel version\n"
+sleep 3
+uname -r
+
+echo -e "\nchecking the hardrive info\n"
+sleep 3
+lsblk
+
+echo -e "\nchecking the os version\n"
+sleep 3
+cat /etc/*release
+
+echo -e "\nchecking the system bits\n"
+sleep 3
+getconf LONG_BIT
